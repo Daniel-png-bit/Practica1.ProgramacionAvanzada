ddde450ef11e42c3dc2d841e68958e649fa10aec
Daniel-png-bit <73356200+Daniel-png-bit@users.noreply.github.com>
2021-03-21 23:48:35 -0400

    Add files via upload
    Subiendo últimas carpetas

---
+++ Ejercicio 2 - Lambda1/Ej2LamdbaString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ejercicio2_Lambda
+{
+    delegate bool CadenaDelegado(char caracter);
+
+    class Program
+    {
+        public static string Filtrar(string cadena, CadenaDelegado del)
+        {
+            string fraseCadena = "";
+            foreach (var caracter in cadena)
+                if (del(caracter))
+                    fraseCadena += caracter;
+            return fraseCadena.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Ingrese una frase: ");
+            var cadena = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Cadena: " + cadena);
+            var vocales = Program.Filtrar(cadena, caracter => caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u' ||
+                                          caracter == 'A' || caracter == 'E' || caracter == 'I' || caracter == 'O' || caracter == 'U');
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"La frase tiene como vocales: {vocales}");
+
+            var dig = Program.Filtrar(cadena, caracter => char.IsDigit(caracter));
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"La frase tiene como dígitos: {dig}");
+
+            var caracMin = Program.Filtrar(cadena, caracter => char.IsLower(caracter));
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"La frase tiene como carácteres minúsculas: {caracMin}");
+
+            var caracNum = Program.Filtrar(cadena, caracter => char.IsLetter(caracter) || char.IsNumber(caracter));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("La frase tiene como carácteres y números: " + caracNum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio1 - Lambda/Ej1LambdaStudents/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+namespace Ejercicio1Lambda
+{
+    public delegate bool Comparacion(int nota);
+
+    public class Estudiante
+    {
+        private string nombre;
+        private int nota;
+
+        public string Nombre { get; set; }
+        public int Nota { get; set; }
+    }
+
+
+    public class Curso
+    {
+        private Estudiante[] estudiantes = new Estudiante[5];
+        public void Cargar(int pos, Estudiante estudiante)
+        {
+            estudiantes[pos] = estudiante;
+        }
+
+        public void MostrarEstudiantes()
+        {
+            foreach (var estudiante in estudiantes)
+            {
+                Console.WriteLine($"Nombre: {estudiante.Nombre} - Nota: {estudiante.Nota}");
+            }
+                
+        }
+
+        public void MostrarIf(Comparacion comparacion)
+        {
+            foreach (var estudiante in estudiantes)
+            {
+                if (comparacion(estudiante.Nota))
+                {
+                    Console.WriteLine($"Nombre: {estudiante.Nombre} - Nota: {estudiante.Nota}");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso c1 = new Curso();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ingrese el nombre del estudiante #{(i+1)}: ");
+                string nameS = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Ingrese la nota del estudiante #{ (i + 1)}: ");
+                int notaS = int.Parse(Console.ReadLine());
+                c1.Cargar(i, new Estudiante
+                {
+                    Nombre = nameS,
+                    Nota = notaS
+                });
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Lista completa de estudiantes.");
+            c1.MostrarEstudiantes();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Lista completa de estudiantes con nota mayor o igual a 51.");
+            c1.MostrarIf((nota) => nota >= 51);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Lista completa de estudiantes que tienen de nota 50.");
+            c1.MostrarIf((nota) => nota == 50);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
